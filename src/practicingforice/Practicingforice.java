5313960928b3ff76e397d53fc01bfe9e4b491abf
pateashl <pateashl@sheridancollege.ca>
2020-06-03 23:38:43 -0400

    Practice Test90

---
+++ src/practicingforice/Practicingforice.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicingforice;
+
+/**
+ *
+ * @author ashle
+ */
+public class Practicingforice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("HIIIIII");
+    }
+    
+}
